ea8eaaf143509f84c395fc8802d2fa541ef2eefe
riverson nobre <riversonnobre14@gmail.comgit>
2024-03-17 17:06:49 -0300

    primeira verção do programa desconto produtos

--- produtos/Program.cs
+++ produtos/Program.cs
@@ -12,13 +12,43 @@ namespace produtos
         {
 
             Console.WriteLine("Incira o nome do produto:");
+            string produto = Console.ReadLine();
+            Console.WriteLine("Incira a quantidade de produtos:");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o preço");
 
 
-            string nome = Console.ReadLine();
+            double preco = double
+                .Parse(Console.ReadLine());
+
+            double precoFinal = preco * quantidade;
+            double desconto1 = precoFinal * 0.9;
+            double desconto2 = precoFinal * 0.8;
+            double desconto3 = precoFinal *  0.75;
 
 
-        
+            if (quantidade <= 10)
+            {
+                Console.WriteLine($"Você escolheu {produto} {quantidade} e o valor total é de {precoFinal}");
+            }
+            else if (quantidade <= 20)
+            {
+                Console.WriteLine($"Você escolheu {quantidade}  com 10%de desconto, com o valor final de {desconto1}");
+
+            }
+
+            else if (quantidade <= 50)
+            {
+
+                Console.WriteLine($" Você comprou {quantidade} com 20% de desconto, que deu {desconto2}");
+            }
+            else if(quantidade>=50)
+            {
+                Console.WriteLine($"Você comprou {quantidade}produtos, com 25%de desconto com o preço final de {desconto3}");
+
+            }
+
+            }
         }
-
     }
-}
